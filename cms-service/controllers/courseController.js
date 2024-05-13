52e97cdb3c86e54072c9663b5e65b24d35e2545a
RxshiA <rashiamarasiri@gmail.com>
2024-05-13 21:57:57 +0530

    Course methods changes: including update and get method changes

--- cms-service/controllers/courseController.js
+++ cms-service/controllers/courseController.js
@@ -1,20 +1,41 @@
 const Course = require('../models/course')
+const Instructor = require('../models/superior')
 
 exports.addCourse = async (req, res) => {
-  const course = new Course({ ...req.body, approved: false });
-  await course.save();
-  res.status(201).send({ message: 'Course added successfully', course: course });
+  try {
+    const instructor = await Instructor.findOne({ email: req.body.email });
+    if (!instructor) {
+      return res.status(404).send({ message: 'Instructor not found' });
+    }
+    const course = new Course({ ...req.body, instructor: instructor._id, approved: false });
+    await course.save();
+    res.status(201).send({ message: 'Course added successfully', course: course });
+  } catch (error) {
+    res.status(500).send({ message: 'An error occurred', error: error });
+  }
 };
 
 exports.updateCourse = async (req, res) => {
   try {
     const course = await Course.findById(req.params.id);
+    if (!course) {
+      return res.status(404).send({ message: 'Course not found' });
+    }
+
     if (req.body.title) course.title = req.body.title;
     if (req.body.content) course.content = req.body.content;
-    if (req.body.instructor) course.instructor = req.body.instructor;
     if (req.body.videos) course.videos = req.body.videos;
     if (req.body.notes) course.notes = req.body.notes;
     if (req.body.quizzes) course.quizzes = req.body.quizzes;
+
+    if (req.body.instructor) {
+      const instructor = await Instructor.findOne({ email: req.body.instructor });
+      if (!instructor) {
+        return res.status(404).send({ message: 'Instructor not found' });
+      }
+      course.instructor = instructor._id;
+    }
+
     course.approved = false;
     await course.save();
     res.status(200).send({ message: 'Course updated successfully', course: course });
@@ -44,20 +65,26 @@ exports.approveCourse = async (req, res) => {
 };
 
 exports.getCourse = async (req, res) => {
-  const course = await Course.findById(req.params.id);
-  if (!course) {
-    res.status(404).send('Course not found');
+  try {
+    const course = await Course.findById(req.params.id).populate('instructor', 'username -_id');
+    if (!course) {
+      return res.status(404).send('Course not found');
+    }
+    res.status(200).send({ message: 'Course retrieved successfully', course: course });
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while retrieving the course."
+    });
   }
-  res.status(200).send({ message: 'Course retrieved successfully', course: course });
 };
 
 exports.getCourses = async (req, res) => {
   try {
-    const courses = await Course.find();
+    const courses = await Course.find().populate('instructor', 'username -_id');
     res.status(200).send({ message: 'Courses retrieved successfully', courses: courses });
   } catch (err) {
     res.status(500).send({
       message: err.message || "Some error occurred while retrieving the courses."
     });
   }
-}
+};
